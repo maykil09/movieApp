3df32f84a03e4abb8adc132c62a965354dc5e30d
Michael Guilaran <maykil093@gmail.com>
2021-09-10 04:22:31 +0800

    update layout

--- resources/js/components/auth/Login.js
+++ resources/js/components/auth/Login.js
@@ -1,35 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const requestLogin = (e) => {
+        e.preventDefault();
+        setIsSubmit(true);
+        let req = { email, password };
+    };
+
     return (
         <div className="bg-white w-full lg:w-1/3 mx-auto rounded-lg lg:my-20 px-4 py-4 shadow-lg">
             <h1 className="text-gray-700 font-bold text-4xl mb-4 text-center ">
                 LOGIN
             </h1>
-            <input
-                type="text"
-                placeholder="Email"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <input
-                type="text"
-                placeholder="Password"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <button
-                className="text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider"
-                style={{ backgroundColor: "#1977f2" }}
-            >
-                Login
-            </button>
-            <div className="flex justify-center my-6">
+            <form onSubmit={requestLogin}>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500 active:bg-blue-700 "
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button
-                    className="text-white h-12 rounded px-6 font-bold"
-                    style={{ backgroundColor: "#54b72b" }}
+                    type="submit"
+                    className={`text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider ${
+                        isSubmit ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                    style={{ backgroundColor: "#1977f2" }}
                 >
-                    Create new Account
+                    Login
                 </button>
-            </div>
+                <div className="flex justify-center my-6">
+                    <Link
+                        to="/register"
+                        className="text-white font-bold py-3 px-5 rounded"
+                        style={{ backgroundColor: "#54b72b" }}
+                    >
+                        Create new Account
+                    </Link>
+                    {/* <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed">
+                        Button
+                    </button> */}
+                </div>
+            </form>
         </div>
     );
 }
--- resources/js/components/auth/Register.js
+++ resources/js/components/auth/Register.js
@@ -1,42 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Register() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordRepeat, setPasswordRepeat] = useState("");
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const submitRegistration = (e) => {
+        e.preventDefault();
+        setIsSubmit(true);
+        let newUser = {
+            firstName,
+            lastName,
+            email,
+            password,
+            passwordRepeat,
+        };
+        console.log(newUser);
+    };
+
     return (
         <div className="bg-white w-full lg:w-1/3 mx-auto rounded-lg lg:my-20 px-4 py-4 shadow-lg">
             <h1 className="text-gray-700 font-bold text-4xl mb-4 text-center ">
                 Register
             </h1>
-            <input
-                type="text"
-                placeholder="First Name"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <input
-                type="text"
-                placeholder="Last Name"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <input
-                type="text"
-                placeholder="Email"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <input
-                type="text"
-                placeholder="Password"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <input
-                type="text"
-                placeholder="Password repeat"
-                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-            />
-            <button
-                className="text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider"
-                style={{ backgroundColor: "#1977f2" }}
-            >
-                Create Account
-            </button>
+            <form onSubmit={submitRegistration}>
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password repeat"
+                    className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                    onChange={(e) => setPasswordRepeat(e.target.value)}
+                />
+                <button
+                    className={`text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider ${
+                        isSubmit ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                    style={{ backgroundColor: "#1977f2" }}
+                >
+                    Create Account
+                </button>
+            </form>
         </div>
     );
 }
