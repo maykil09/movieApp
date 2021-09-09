2634e5dd274f934f605373bc8b2a919746bebf92
Michael Guilaran <maykil093@gmail.com>
2021-09-10 03:40:54 +0800

    auth layout

--- resources/js/components/auth/Register.js
+++ resources/js/components/auth/Register.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function Login() {
+function Register() {
     return (
         <div className="bg-white w-full lg:w-1/3 mx-auto rounded-lg lg:my-20 px-4 py-4 shadow-lg">
             <h1 className="text-gray-700 font-bold text-4xl mb-4 text-center ">
-                LOGIN
+                Register
             </h1>
+            <input
+                type="text"
+                placeholder="First Name"
+                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+            />
             <input
                 type="text"
                 placeholder="Email"
@@ -16,22 +26,19 @@ function Login() {
                 placeholder="Password"
                 className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
             />
+            <input
+                type="text"
+                placeholder="Password repeat"
+                className="w-full mb-3 px-4 py-3 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+            />
             <button
                 className="text-white py-3 rounded-lg w-full font-bold text-xl tracking-wider"
                 style={{ backgroundColor: "#1977f2" }}
             >
-                Login
+                Create Account
             </button>
-            <div className="flex justify-center my-6">
-                <button
-                    className="text-white h-12 rounded px-6 font-bold"
-                    style={{ backgroundColor: "#54b72b" }}
-                >
-                    Create new Account
-                </button>
-            </div>
         </div>
     );
 }
 
-export default Login;
+export default Register;
