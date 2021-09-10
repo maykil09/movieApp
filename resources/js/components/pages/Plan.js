31132509a5f8a332990b18e6403cba43f4f528dc
Michael Guilaran <maykil093@gmail.com>
2021-09-10 10:24:33 +0800

    minor update

--- resources/js/components/pages/Plan.js
+++ resources/js/components/pages/Plan.js
@@ -14,9 +14,6 @@ function Plan() {
             <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                 Pricing
             </h1>
-            <div className="w-full mb-4">
-                <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-            </div>
 
             <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
                 <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
